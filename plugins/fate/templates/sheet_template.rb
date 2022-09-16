20eaa77065c95f3c6c3eb876f0cbebf849955ed4
AriesOwl <112923743+AriesOwl@users.noreply.github.com>
2022-09-16 13:06:34 -0700

    Add files via upload

--- plugins/fate/templates/sheet_template.rb
+++ plugins/fate/templates/sheet_template.rb
@@ -29,6 +29,18 @@ module AresMUSH
         "#{linebreak}#{left(name, 16)} #{left(rating_text, 20)}"
       end
       
+      def hc
+        (@char.fate_hc || []).sort
+      end
+
+      def tr 
+        (@char.fate_tr || []).sort
+      end
+
+      def ori 
+        (@char.fate_ori || []).sort
+      end
+
       def aspects
         (@char.fate_aspects || []).sort
       end
