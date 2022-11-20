72848e50d714b3d6c238c1b915a6ab28390e72ce
AriesOwl <112923743+AriesOwl@users.noreply.github.com>
2022-11-20 05:03:27 -0800

    Add files via upload

--- plugins/fate/public/fate_model.rb
+++ plugins/fate/public/fate_model.rb
@@ -6,7 +6,7 @@ module AresMUSH
     attribute :fate_aspects, :type => DataType::Array, :default => []
     attribute :fate_stunts, :type => DataType::Hash, :default => {}
     attribute :fate_skills, :type => DataType::Hash, :default => {}
-    attribute :fate_pstress 
+    attribute :fate_pstress :type => DataType::Array, :default => []
     attribute :fate_points, :type => DataType::Integer
     attribute :fate_refresh, :type => DataType::Integer
   end
