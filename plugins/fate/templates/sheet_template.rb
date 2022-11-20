50f3aff20a2b392be663da1af3965c806712d436
AriesOwl <112923743+AriesOwl@users.noreply.github.com>
2022-11-19 21:41:32 -0800

    Add files via upload

--- plugins/fate/templates/sheet_template.rb
+++ plugins/fate/templates/sheet_template.rb
@@ -48,6 +48,11 @@ module AresMUSH
       def stunts
         (@char.fate_stunts || {}).sort
       end
+
+      def stress
+        (@char.physical_stress_thresh || []).sort
+        (@char.mental_stress_thresh || []).sort
+      end
     end
   end
 end
